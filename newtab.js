c89e37bbfa21792c5ed2d4639e738fb600c2a5d7
Bedanta Dey <135055975+bedantadey17@users.noreply.github.com>
2023-07-06 00:10:39 +0530

    initial release

--- newtab.js
+++ newtab.js
@@ -1,21 +1,50 @@
-const searchButton = document.getElementById('searchButton');
+document.addEventListener('DOMContentLoaded', function () {
+  const searchButton = document.getElementById('searchButton');
+  const searchInput = document.getElementById('searchInput');
+  const darkModeToggle = document.getElementById('darkModeToggle');
 
-searchButton.addEventListener('click', () => {
-  const searchInput = document.getElementById('searchInput').value;
-  const searchEngine = document.getElementById('searchEngine').value;
+  searchButton.addEventListener('click', function () {
+    performSearch();
+  });
 
-  let url;
-  if (searchEngine === 'google') {
-    url = `https://www.google.com/search?q=${encodeURIComponent(searchInput)}`;
-  } else if (searchEngine === 'wikipedia') {
-    url = `https://en.wikipedia.org/wiki/${encodeURIComponent(searchInput)}`;
-  } else if (searchEngine === 'youtube') {
-    url = `https://www.youtube.com/results?search_query=${encodeURIComponent(searchInput)}`;
-  } else if (searchEngine === 'bing') {
-    url = `https://www.bing.com/search?q=${encodeURIComponent(searchInput)}`;
-  } else if (searchEngine === 'ddg') {
-    url = `https://duckduckgo.com/?q=${encodeURIComponent(searchInput)}`;
+  searchInput.addEventListener('keyup', function (event) {
+    if (event.key === 'Enter') {
+      performSearch();
+    }
+  });
+
+  darkModeToggle.addEventListener('click', function () {
+    const isDarkMode = document.body.classList.toggle('dark-mode');
+    chrome.storage.sync.set({ darkMode: isDarkMode });
+  });
+
+  // Retrieve dark mode preference on extension load
+  chrome.storage.sync.get('darkMode', function (result) {
+    const isDarkMode = result.darkMode;
+    if (isDarkMode !== undefined) {
+      document.body.classList.toggle('dark-mode', isDarkMode);
+    }
+  });
+
+  function performSearch() {
+    const searchValue = searchInput.value;
+    const searchEngine = document.getElementById('searchEngine').value;
+
+    let url;
+    if (searchEngine === 'google') {
+      url = `https://www.google.com/search?q=${encodeURIComponent(searchValue)}`;
+    } else if (searchEngine === 'wikipedia') {
+      url = `https://en.wikipedia.org/wiki/${encodeURIComponent(searchValue)}`;
+    } else if (searchEngine === 'youtube') {
+      url = `https://www.youtube.com/results?search_query=${encodeURIComponent(searchValue)}`;
+    } else if (searchEngine === 'bing') {
+      url = `https://www.bing.com/search?q=${encodeURIComponent(searchValue)}`;
+    } else if (searchEngine === 'ddg') {
+      url = `https://duckduckgo.com/?q=${encodeURIComponent(searchValue)}`;
+    }
+
+    if (url) {
+      window.location.href = url;
+    }
   }
-
-  window.location.href = url;
 });
